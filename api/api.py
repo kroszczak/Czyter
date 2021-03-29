c5fc4d43f61a2ba5d630c1cd33dde3278cc5d74d
krylowicz <kacper.krylowicz@uczen.zsk.poznan.pl>
2021-03-29 20:12:38 +0200

    add file upload form

--- api/api.py
+++ api/api.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 @app.route('/api', methods=['GET'])
 def api():
     return {
-            'userID': 3,
-            'title': 'The App',
-            'state': "in progress"
-        }
+        'userID': 3,
+        'title': 'The App',
+        'state': "in progress"
+    }
         
 if __name__ == '__main__':
     app.run()
--- src/App.js
+++ src/App.js
@@ -1,20 +1,55 @@
-import {useState, useEffect} from 'react'
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState('');
+  const [file, setFile] = useState(null);
 
-  const [initialData, setInitial] = useState([{}])
+  const handleFileUpload = e => {
+    const file = e.target.files[0]
+    const ext = file.split('.').pop();
+    const size = file.size;
 
-  useEffect(() => {
-    fetch('/api').then(res => res.json()
-    ).then(res => setInitial(res))
-  }, [])
-  
+    if (ext == 'pdf' && size < 30) {
+      setFile(file);
+    } else {
+      alert('error');
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData()
+    formData.append('title', title);
+    formData.append('file', file);
+
+    fetch(
+      '/upload',
+      {
+        method: 'POST',
+        body: formData
+      }
+    )
+      .then(res => res.json())
+      .then(res => console.log('success', res))
+      .catch(err => console.error('error', err));
+  }
+
   return (
     <div className="App">
-
-      <h1>{initialData.title}</h1>
-      <h1>{initialData.state}</h1>
+      <form>
+        <input
+          type="text"
+          value={title}
+          onChange={e => setTitle(e)}
+        />
+        <input
+          type="file"
+          accept="application/pdf"
+          onChange={e => handleFileUpload(e)}
+        />
+        <button onClick={e => handleSubmit(e)}>upload</button>
+      </form>
     </div>
   );
 }
